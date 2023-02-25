fab7ca803975038eb40ee0137e30cfe5dd1d0fd0
virtualnature101 <virtualnature101@gmail.com>
2023-02-25 23:33:36 +0530

    database function changes

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,7 +51,7 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value: string) {
+  search(value: string): void {
     let foundItems = [];
     if (value.length <= 0) {
       this.getStudentData();
