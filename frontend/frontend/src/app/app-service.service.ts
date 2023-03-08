5f85ec43c648af21fee4713f8f121a9344329642
virtualnature101 <virtualnature101@gmail.com>
2023-03-08 11:23:26 +0530

    database function changes

---
+++ frontend/frontend/src/app/app-service.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppServiceService {
+
+  readonly ROOT_URL;
+
+  constructor(private http: HttpClient) {
+    if(environment.production == false){
+      this.ROOT_URL = 'test'
+    }else{
+      this.ROOT_URL = 'http://localhost:8080'
+    }
+  }
+
+  initializeDB(){
+    return this.http.get(`/api/dbinitialize`)
+  }
+
+  getTeacherData(){
+    return this.http.get(`/api/listTeachers`)
+  }
+
+  getStudentData(){
+    return this.http.get(`/api/listStudents`)
+  }
+
+  getOneStudentData(payload: Object){
+    return this.http.post(`/api/getStudentInfo`, payload)
+  }
+
+  getOneTeacherData(payload: Object){
+    return this.http.post(`/api/getTeacherInfo`, payload)
+  }
+
+  addTeacher(payload: Object){
+    return this.http.post(`/api/addTeacher`, payload)
+  }
+
+  deleteTeacher(payload: Object){
+    return this.http.post(`/api/deleteTeacher`, payload)
+  }
+
+  editTeacher(payload: Object){
+    return this.http.post(`/api/editTeacher`, payload)
+  }
+
+  editStudent(payload: Object){
+    return this.http.post(`/api/editStudent`, payload)
+  }
+
+  addStudent(payload: Object){
+    return this.http.post(`/api/addStudent`, payload)
+  }
+
+  deleteStudent(payload: Object){
+    return this.http.post(`/api/deleteStudent`, payload)
+  }
+}
